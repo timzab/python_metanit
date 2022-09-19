442fa8ed0095c509c371cae9640d9ade464815d1
timzab <a_t_i@mail.ru>
2022-09-19 22:16:39 +0300

    Add new functions

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 def plus(a,b):
-    return (a+b)
+    return a + b
 def minus(a,b):
-    return (a-b)
+    return a - b
+def multy(a,b):
+    return a * b
+
+def devide(a,b):
+    return a / b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print(plus(2, 3))
     print(minus(3, 2))
+    print(multy(3, 2))
+    print(devide(3, 2))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
